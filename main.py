e6417e64ceccefd883ba7dc5a885b821ff34ed30
Georgi Iliev <georgi.iliev533@outlook.com>
2023-03-15 00:37:11 +0200

    Completed to working state

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-import selenium.common.exceptions
-from webbot import Browser
 import time
-from lxml import etree
-from botcity.web import WebBot
+import requests
+import string
+import random
+from botcity.web import WebBot, Browser
 from botcity.web.browsers.chrome import default_options
 from botcity.web import By
 
+USER = "GOSHO"
+WORDS_CACHE = dict()
+USER_AGENT = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+}
 
-class Browser(WebBot):
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+class MyBrowser(WebBot):
     def action(self, execution=None):
         self.headless = False
         self.browser = Browser.CHROME
@@ -22,12 +32,33 @@ class Browser(WebBot):
         self.options = def_options
 
 
+def get_word(syllable: str) -> str:
+    if syllable not in WORDS_CACHE.keys():
+        data = requests.get(
+            url='https://api.datamuse.com/words?sp=*{0}*'.format(syllable),
+            headers=USER_AGENT
+        ).json()
+
+        words = list()
+        for item in data:
+            words.append(item['word'])
+
+        WORDS_CACHE[syllable] = dict()
+        WORDS_CACHE[syllable]["words"] = words
+        WORDS_CACHE[syllable]["last"] = 0
+    else:
+        WORDS_CACHE[syllable]["last"] += 1
+    return WORDS_CACHE[syllable]["words"][WORDS_CACHE[syllable]["last"]]
+
+
 def func(url: str):
     global bot
-    bot = Browser()
+    bot = MyBrowser()
     bot.browse(url)
     time.sleep(1)
-    bot.kb_type("GOSSHO")
+    global USER
+    USER += id_generator(size=3)
+    bot.kb_type(USER)
     bot.enter()
     time.sleep(1)
     bot.add_image(label="join", path='./images/join.png')
@@ -37,9 +68,22 @@ def func(url: str):
 
     iframe = bot.find_element('/html/body/div[2]/div[4]/div[1]/iframe', By.XPATH)
     bot.enter_iframe(iframe)
-    time.sleep(1)
+    time.sleep(3)
 
-    print(bot.find_element('/html/body/div[2]/div[2]/div[2]/div[2]/div', By.XPATH).text)
+    while True:
+        turn = bot.find_element('/html/body/div[2]/div[3]/div[2]/div[1]/span[1]', By.XPATH).text
+        if turn == "":
+            syllable = bot.find_element('/html/body/div[2]/div[2]/div[2]/div[2]/div', By.XPATH).text
+            word = get_word(syllable)
+            print(word)
+            bot.kb_type(word)
+            bot.enter()
+        winner = bot.find_element('/html/body/div[2]/div[2]/div[2]/div[3]/div/div[2]', By.XPATH).text
+        if winner != '':
+            break
+        time.sleep(1)
+
 
 if __name__ == '__main__':
-    func('https://jklm.fun/UAKW')
+    url = input()
+    func(url)
